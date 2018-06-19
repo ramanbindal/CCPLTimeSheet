538aa96a1bc220b4efde1e3e3b3f603a22cb8ef9
ramanbindal <raman.bindal@cubastion.com>
2018-06-19 18:56:52 +0530

    Db added

--- app/src/main/java/com/example/cub11/ccpltimesheet/MainActivity.java
+++ app/src/main/java/com/example/cub11/ccpltimesheet/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.cub11.ccpltimesheet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.cub11.ccpltimesheet.database.DbOpenHelper;
+
 public class MainActivity extends AppCompatActivity {
+    private DbOpenHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db=new DbOpenHelper(this);
     }
 }
