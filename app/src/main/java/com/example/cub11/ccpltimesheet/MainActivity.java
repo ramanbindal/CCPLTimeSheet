125afd0f5e044a8b97733187267827a65f1828fe
ramanbindal <raman.bindal@cubastion.com>
2018-06-19 17:12:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cub11/ccpltimesheet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cub11.ccpltimesheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
